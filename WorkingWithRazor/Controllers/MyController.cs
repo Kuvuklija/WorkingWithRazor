55411adcda59cd371ec167209463272256bb17de
Kuvuklija <43697762+Kuvuklija@users.noreply.github.com>
2019-01-14 17:20:50 +0300

    razor
    razor

---
+++ WorkingWithRazor/Controllers/MyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class MyController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult Time(DateTime time){
+            return View(time);
+        }
+    }
+}
---
+++ WorkingWithRazor/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using WorkingWithRazor.Infrastructure;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WorkingWithRazor
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new CustomLocationViewEngine());
+        }
+    }
+}
